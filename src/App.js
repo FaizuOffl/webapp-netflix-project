a1b7ae7fbafb998aea4acb5e5221ac63dd315270
Mohammed Fahim Ahmed <faizu.offl@gmail.com>
2022-11-18 17:29:47 +0400

    Initial Commit & Removed Basic React Config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/nav/Nav';
+import Header from './components/header/Header';
+import Content from './components/content/Content';
+import axios from 'axios';
 
-function App() {
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+axios.defaults.params = {
+  api_key: '00cc4b4770c22d9316433c56df5c5eae',
+};
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />
+      <Header />
+      <Content />
     </div>
   );
-}
+};
 
 export default App;
